b5a6a3f18c8aba47a44611488fced309b78fd6fc
Connor <connordbrown@gmail.com>
2024-04-02 11:33:29 -0600

    Ch11Program1 part B

--- Ch11Program1.c
+++ Ch11Program1.c
@@ -37,6 +37,42 @@ int main(void) {
   // close file
   fclose(fp);
   puts("File initialized with 100 empty records.");
+  
+
+  // ########## b) ###########
+
+  // open file for editing
+  fp = fopen("nameage.dat", "rb+");
+  if (fp == NULL) {
+    puts("Error opening file.");
+    return 1;
+  }
+
+  // add 10 records
+  for (int i = 0; i < 10; ++i) {
+    struct person newPerson;
+
+    puts(""); // spacing
+    
+    // get last name
+    printf("%s", "Enter last name: ");
+    scanf("%14s", newPerson.lastName);
+    // get first name
+    printf("%s", "Enter first name: ");
+    scanf("%14s", newPerson.firstName);
+    // get age
+    printf("%s", "Enter age: ");
+    scanf( "%3s", newPerson.age);
+
+    puts(""); // spacing
+    
+    // write record to file
+    fwrite(&newPerson, sizeof(struct person), 1, fp);
+  }
+
+  // close file
+  fclose(fp);
+  puts("10 records written to the file");
 
   return 0;
 }
